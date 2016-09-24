909feeda096fbe76ff89e6a014dde641f7e3f9ca
sboursault <sebastien.boursault@gmail.com>
2016-09-24 14:44:43 +0200

    improve alignments and gridflow

--- client/main.js
+++ client/main.js
@@ -7,10 +7,10 @@ const jiraUrl = 'https://jira.atlassian.com';
 
 Template.body.helpers({
   jiraQueryMonitors: [
-    { id: 'product1', title: 'product A', jql: 'assignee="copain"' },
-    { id: 'product2', title: 'product B', jql: '' },
-    { id: 'product3', title: 'product C', jql: 'priority in (High, Medium)' },
-    { id: 'product4', title: 'product D', jql: 'priority in (Medium, Low)' }
+    { monitorId: 'product1', title: 'product A', jql: 'assignee="copain"' },
+    { monitorId: 'product2', title: 'product B', jql: '' },
+    { monitorId: 'product3', title: 'product C', jql: 'priority in (High, Medium)' },
+    { monitorId: 'product4', title: 'product D', jql: 'priority in (Medium, Low)' }
   ],
   refreshMonitor: function() {
     JqlMonitorUi.refresh(this.monitorId, jiraUrl, this.jql);
