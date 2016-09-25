e1e1c52b73a696fb1289db764dd092e063141d83
sboursault <sebastien.boursault@gmail.com>
2016-09-25 22:18:48 +0200

    search based on affects version found via api call

--- client/main.js
+++ client/main.js
@@ -1,18 +1,60 @@
 import { Template } from 'meteor/templating';
+import { ClientUtils } from '../imports/utils/client-utils.js';
 import { JqlMonitorUi } from '../imports/jira-query-monitor/ui.js';
 
 import './main.html';
 
 const jiraUrl = 'https://jira.atlassian.com';
+const affectsVersionParam = ClientUtils.getUrlParams()['affectsVersion'];
+const jiraProject = ''
+var baseFilter = 'resolution = Unresolved';
+
+const jiraQueryMonitorArray = [
+  { monitorId: 'product1', title: 'product A', jql: 'assignee="copain"' },
+  { monitorId: 'product2', title: 'product B', jql: 'priority in (Low)' },
+  { monitorId: 'product3', title: 'product C', jql: 'priority in (High, Medium)' },
+  { monitorId: 'product4', title: 'product D', jql: 'priority in (Medium, Low)' }
+];
+
+Template.body.onRendered(function () {
+  if (affectsVersionParam) {
+    var regex = affectsVersionParam.replace('.', '\\.') + '(\\D.*)?$';
+    Meteor.call('jira.versions', jiraUrl, jiraProject, regex,
+      function onComplete(err, versions) {
+        if (err) {
+          console.error(err);
+        } else {
+          versions = (versions || []).map(
+            function decorate(version) {
+              return '"' + version + '"';
+            });
+          console.log('found matching versions: ' + versions.toString());
+          baseFilter = (baseFilter || '') + ' AND affectedVersion in (' + versions.toString() + ')';
+          window.dispatchEvent(new CustomEvent('jqlMonitor:affectsVersion:update', {}));
+          console.log('jqlBaseFilter:affectsVersion:update triggered');
+        }
+      }
+    );
+  }
+})
 
 Template.body.helpers({
-  jiraQueryMonitors: [
-    { monitorId: 'product1', title: 'product A', jql: 'assignee="copain"' },
-    { monitorId: 'product2', title: 'product B', jql: '' },
-    { monitorId: 'product3', title: 'product C', jql: 'priority in (High, Medium)' },
-    { monitorId: 'product4', title: 'product D', jql: 'priority in (Medium, Low)' }
-  ],
+  affectsVersion: function() {
+    return affectsVersionParam ? ('on ' + affectsVersionParam) : '';
+  },
+  jiraQueryMonitors: function() {
+    return jiraQueryMonitorArray;
+  },
   refreshMonitor: function() {
-    JqlMonitorUi.refresh(this.monitorId, jiraUrl, this.jql);
-  }
+    var jiraQueryMonitor = this;
+    var monitorId = jiraQueryMonitor.monitorId, jql = jiraQueryMonitor.jql;
+    if (affectsVersionParam) {
+      window.addEventListener('jqlMonitor:affectsVersion:update', function () {
+        JqlMonitorUi.refresh(monitorId, jiraUrl, baseFilter + ( jql ? ' and ' + jql : ''));
+      });
+    } else {
+      JqlMonitorUi.refresh(monitorId, jiraUrl, baseFilter + ( jql ? ' and ' + jql : ''));
+    }
+  },
 });
+
--- imports/jira-query-monitor/ui.js
+++ imports/jira-query-monitor/ui.js
@@ -1,23 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 
-
 export const JqlMonitorUi = {
   refresh: function(monitorId, jiraUrl, jql) {
-    console.log('search in jira with "' + jql + '" (' + monitorId + ')');
-    /*p.addEventListener("receivedJqlResults", function(e) {
-        if (e.monitorId === this.id) {
-        }
-    })*/
+    console.log('call jira.search with jql: "' + jql + '" (monitor for "' + monitorId + '")');
     Meteor.call('jira.search', jiraUrl, jql, function onComplete(err, jqlMonitorData) {
       if (err) {
         console.error(err);
       } else {
         console.log(jqlMonitorData);
-         //document.dispatchEvent(new CustomEvent("receivedJqlResults", {monitorId: monitorId, results: data}));
         var monitorEl = document.getElementById(monitorId);
         monitorEl.innerHTML = jqlMonitorData.asHtml;
         monitorEl.className = 'monitor monitor-' + jqlMonitorData.temperature;
-        monitorEl.setAttribute('title', jqlMonitorData.issueKeys.toString());
+        monitorEl.setAttribute('title', jqlMonitorData.issueKeys.toString().replace(',', ', '));
       }
     });
   }
